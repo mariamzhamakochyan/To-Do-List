59d952ab7fcfcbd16ac9a5b61c18100d2d3df9a7
Mariam Zhamakochyan <mariamzhamakochyan@gmail.com>
2023-06-14 02:27:44 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,88 +1,120 @@
-from flask import Flask, render_template, request, redirect, jsonify, g
 from datetime import datetime, timedelta
-import sqlite3
 
-app = Flask(__name__)
+tasks = []
 
-DATABASE = 'tasks.db'
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
-
-def create_table():
-    db = get_db()
-    db.execute("CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, description TEXT NOT NULL, deadline DATETIME, completed INTEGER DEFAULT 0)")
-    db.commit()
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/api/tasks')
-def get_tasks():
-    create_table()
-    tasks = []
-    cur = get_db().execute("SELECT id, description, deadline, completed FROM tasks")
-    for row in cur.fetchall():
-        task = {
-            'id': row[0],
-            'description': row[1],
-            'deadline': row[2],
-            'completed': bool(row[3])
-        }
-        tasks.append(task)
-    return jsonify(tasks)
-
-@app.route('/api/tasks', methods=['POST'])
 def add_task():
-    description = request.form.get('description')
-    deadline = request.form.get('deadline')
+    task = input("Enter a new task: ")
+    deadline = input("Enter the task deadline (YYYY-MM-DD HH:MM): ")
+    deadline = datetime.strptime(deadline, "%Y-%m-%d %H:%M")
+    tasks.append({"task": task, "deadline": deadline, "completed": False})
+    print("Task added successfully!")
 
-    db = get_db()
-    db.execute("INSERT INTO tasks (description, deadline) VALUES (?, ?)", (description, deadline))
-    db.commit()
+def delete_task():
+    view_tasks()
+    if not tasks:
+        return
 
-    return redirect('/')
+    task_number = int(input("Enter the task number to delete: "))
+    if task_number <= 0 or task_number > len(tasks):
+        print("Invalid task number.")
+    else:
+        removed_task = tasks.pop(task_number - 1)
+        print(f"Task '{removed_task['task']}' removed successfully!")
 
-@app.route('/api/tasks/<int:task_id>/complete', methods=['POST'])
-def complete_task(task_id):
-    db = get_db()
-    db.execute("UPDATE tasks SET completed = 1 WHERE id = ?", (task_id,))
-    db.commit()
-    return redirect('/')
+def modify_task():
+    view_tasks()
+    if not tasks:
+        return
 
-@app.route('/api/tasks/<int:task_id>/delete', methods=['POST'])
-def delete_task(task_id):
-    db = get_db()
-    db.execute("DELETE FROM tasks WHERE id = ?", (task_id,))
-    db.commit()
-    return redirect('/')
+    task_number = int(input("Enter the task number to modify: "))
+    if task_number <= 0 or task_number > len(tasks):
+        print("Invalid task number.")
+    else:
+        task = tasks[task_number - 1]
+        new_task = input("Enter the new task: ")
+        task["task"] = new_task
+        print("Task modified successfully!")
 
-@app.before_request
-def check_deadlines():
-    if request.endpoint != 'static':
-        create_table()
-        cur = get_db().execute("SELECT id, deadline, completed FROM tasks")
-        for row in cur.fetchall():
-            task_id = row[0]
-            deadline = datetime.strptime(row[1], '%Y-%m-%d %H:%M:%S')
-            completed = bool(row[2])
+def mark_task_as_completed():
+    view_tasks()
+    if not tasks:
+        return
 
-            if not completed:
-                current_time = datetime.now()
-                time_difference = deadline - current_time
+    task_number = int(input("Enter the task number to mark as completed: "))
+    if task_number <= 0 or task_number > len(tasks):
+        print("Invalid task number.")
+    else:
+        task = tasks[task_number - 1]
+        if not task["completed"]:
+            task["completed"] = True
+            print("Task marked as completed!")
+        else:
+            print("Task is already marked as completed.")
 
-                if time_difference < timedelta(hours=1):
-                    print(f'Task {task_id} approaching deadline!')
+def view_tasks():
+    if not tasks:
+        print("No tasks in the list.")
+    else:
+        print("Tasks:")
+        for index, task in enumerate(tasks):
+            status = "[completed]" if task["completed"] else "[incomplete]"
+            deadline = task["deadline"]
+            time_remaining = deadline - datetime.now()
+            if time_remaining < timedelta(days=1) and not task["completed"]:
+                if time_remaining < timedelta(hours=1):
+                    print(f"{index+1}. {status} {task['task']} (Deadline approaching! Less than an hour remaining)")
+                else:
+                    print(f"{index+1}. {status} {task['task']} (Deadline tomorrow!)")
+            else:
+                print(f"{index+1}. {status} {task['task']}")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+def show_task_stats():
+    total_tasks = len(tasks)
+    completed_tasks = sum(task["completed"] for task in tasks)
+    uncompleted_tasks = total_tasks - completed_tasks
+    print(f"Total tasks: {total_tasks}")
+    print(f"Completed tasks: {completed_tasks}")
+    print(f"Uncompleted tasks: {uncompleted_tasks}")
+
+def check_deadline_approaching():
+    for task in tasks:
+        if not task["completed"]:
+            time_remaining = task["deadline"] - datetime.now()
+            if time_remaining < timedelta(hours=1) and time_remaining > timedelta():
+                print(f"Task '{task['task']}' deadline is approaching! Less than an hour remaining.")
+
+def main():
+    while True:
+        print("\n       ~To-Do List~        ")
+        print("1. Add Task")
+        print("2. View Tasks")
+        print("3. Modify Task")
+        print("4. Delete Task")
+        print("5. Mark Task as Completed")
+        print("6. Show Task Statistics")
+        print("7. Check Deadline Approaching")
+        print("8. Exit")
+        choice = input("Enter your choice (1-8): ")
+
+        if choice == "1":
+            add_task()
+        elif choice == "2":
+            view_tasks()
+        elif choice == "3":
+            modify_task()
+        elif choice == "4":
+            delete_task()
+        elif choice == "5":
+            mark_task_as_completed()
+        elif choice == "6":
+            show_task_stats()
+        elif choice == "7":
+            check_deadline_approaching()
+        elif choice == "8":
+            print("Exit!")
+            break
+        else:
+            print("Try again.")
+
+if __name__ == "__main__":
+    main()
